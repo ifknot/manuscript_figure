9d71830aa90521a37a83765de5ae167d923585d0
headwedge <headwedge@hotmail.co.uk>
2020-11-15 21:45:02 +0000

    documentation

--- fig/component.h
+++ fig/component.h
@@ -1,17 +1,32 @@
 /*****************************************************************//**
  * @file   component.h
  * @brief  The component interface of the composite pattern
- * 
+ * The figure framework specifically uses orientated bounding boxes (OOB).
+ * Ones that, unlike axis aligned bouncing boxes (AABB), are unique to themselves as a 
+ * local coordinate system, range [0.0, 1.0] for both x & y with the origin at top left.
+ * Whilst AABBs are used traditionally, being simpler to test for intersection than OBBs, 
+ * a manuscript graphic figure composed of sub-components benefits greatly from the OBB approach.
+ *
  * @author Jeremy Thornton
  * @date   November 2020
  *********************************************************************/
 #pragma once
 
+#include "rectangle.h"
+
 namespace fig {
 
+	template<typename GDC>
 	class component {
 
+	public:
 
+	private:
+
+		/**
+		 * Specifically an orientated bounding box (OOB).
+		 */
+		rectangle bounding_box;
 
 
 	};
