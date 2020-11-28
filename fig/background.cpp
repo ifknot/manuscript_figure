ca71b1418c663dfb0b483c1476ffa2a4a2998220
headwedge <headwedge@hotmail.co.uk>
2020-11-28 23:23:22 +0000

    dpi for journals

--- fig/background.cpp
+++ fig/background.cpp
@@ -7,7 +7,7 @@ namespace fig {
 	void background::render_traverse(GDC_type& gdc) {
 
 		draw_circle(gdc, { 0.5, 0.5 }, 0.5, {});
-		draw_circle(gdc, { 0.5, 0.5 }, 0.3, {red, yellow, 3, linetypes::dashed});
+		draw_circle(gdc, { 0.4, 0.4 }, 0.3, {red, yellow, 10, linetypes::dotdash});
 
 	}
 
--- test_app.h
+++ test_app.h
@@ -22,13 +22,32 @@ private:
 	std::unique_ptr<test_window> f;
 
 	/**
+	 * 95 dpi (LG monitor) figure 10 x 10 cm 
+	 */
+	//fig::metrics_t metrics{ 95, { 0, 0, 10, 10, fig::units::cm } };
+
+	/**
+	 * 142 dpi (Lenovo p52) figure 10 x 10 cm
+	 */
+	//fig::metrics_t metrics{ 142, { 0, 0, 10, 10, fig::units::cm } };
+
+	/**
 	 * 150 dpi figure 5.2 x 5.2 inches.
 	 */
 	//fig::metrics_t metrics{ 150, { 0, 0, 5.2, 5.2, fig::units::inch } };
 
 	/**
-	 * 76 dpi figure 10 x 10 cm.
+	 * 300 dpi (PLOS-1) figure 5.2 x 5.2 inches.
 	 */
-	fig::metrics_t metrics{ 76, { 0, 0, 10, 10, fig::units::cm } };
+	//fig::metrics_t metrics{ 150, { 0, 0, 5.2, 5.2, fig::units::inch } };
 
+	/**
+	 * 600 dpi (JCI) figure 5.2 x 5.2 inches.
+	 */
+	//fig::metrics_t metrics{ 150, { 0, 0, 5.2, 5.2, fig::units::inch } };
+	
+	/**
+	 * 1200 dpi (Cambridge) figure 3 x 3 inches.
+	 */
+	//fig::metrics_t metrics{ 1200, { 0, 0, 3, 3, fig::units::inch } };
 };
